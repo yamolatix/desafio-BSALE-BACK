f8cb6e2fded0560248a0bcdd782f02ed2fcbd1ea
yamolatix <yamilaortiztoscano@gmail.com>
2022-10-16 10:28:46 -0300

    fix cors error

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(volleyball)
 app.use(cors())
 app.use((req, res, next) => {
 
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://desafio-bsale-front-one.vercel.app/');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
